fe7b92879517cd3a5fb80ef9c98ee5b507bcac4e
PahaN47 <pahan4781@gmail.com>
2023-09-18 01:39:54 +0300

    config change

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import tsconfig from './tsconfig.json';
 
 const SRC_PATH = path.resolve(__dirname, 'src');
