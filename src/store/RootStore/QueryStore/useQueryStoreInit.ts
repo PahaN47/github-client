6eaa13a53ebe19456c901e671d7e379e526b0115
PahaN47 <pahan4781@gmail.com>
2023-09-18 19:30:44 +0300

    a bug. now there is a warning

--- src/store/RootStore/QueryStore/useQueryStoreInit.ts
+++ src/store/RootStore/QueryStore/useQueryStoreInit.ts
@@ -1,15 +1,10 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { useLocation } from 'react-router-dom';
 import { RootStoreContext } from 'store/RootStore';
 
 export const useQueryStoreInit = () => {
   const { search } = useLocation();
   const { setQueryString } = useContext(RootStoreContext).query;
-  const [shouldUpdateQuery, setShouldUpdateQuery] = useState(true);
 
-  useEffect(() => () => setShouldUpdateQuery(false), []);
-
-  if (shouldUpdateQuery) {
-    setQueryString(search);
-  }
+  setQueryString(search);
 };
